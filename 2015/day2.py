35785df433e2635bd448c9b15cdb2f8be3598eb0
calle <ozmehak.calle@gmail.com>
2023-08-23 05:49:50 +0200

    day2 complete, start day3

--- 2015/day2.py
+++ 2015/day2.py
@@ -9,8 +9,23 @@ with open('day2_data.txt', 'r') as file:
 
 lines = data.split('\n')
 
-
+paper_needed = 0
+ribbon_needed = 0
 
 for line in lines:
-    x = line.split('x')
-    print([x])
+    measurements = line.split('x')
+    if len(measurements) == 3:
+        l, w, h = map(int, measurements)
+        surface_area = 2*(l*w) + 2*(w*h) + 2*(h*l)
+        extra = min((l*w), (w*h), (h*l))
+        paper_needed += surface_area + extra
+
+        # ribbon, sum of 2 smallest measurements and add l*w*h 
+        smallest_sum = (2*l) + (2*w) + (2*h) - (max(l, w, h) * 2) 
+        bow = (l*w*h)
+        ribbon_needed += smallest_sum + bow
+    else:
+        pass
+    
+print('paper:', paper_needed)
+print('ribbon:', ribbon_needed)
