b3d983708dbcc0a88aa3f92fbc4b80b58f2aeb42
calle <ozmehak.calle@gmail.com>
2023-08-24 03:20:52 +0200

    made day2 readable

--- 2015/day2.py
+++ 2015/day2.py
@@ -9,23 +9,27 @@ with open('day2_data.txt', 'r') as file:
 
 lines = data.split('\n')
 
-paper_needed = 0
-ribbon_needed = 0
+def part1_and2():
 
-for line in lines:
-    measurements = line.split('x')
-    if len(measurements) == 3:
-        l, w, h = map(int, measurements)
-        surface_area = 2*(l*w) + 2*(w*h) + 2*(h*l)
-        extra = min((l*w), (w*h), (h*l))
-        paper_needed += surface_area + extra
+    paper_needed = 0
+    ribbon_needed = 0
 
-        # ribbon, sum of 2 smallest measurements and add l*w*h 
-        smallest_sum = (2*l) + (2*w) + (2*h) - (max(l, w, h) * 2) 
-        bow = (l*w*h)
-        ribbon_needed += smallest_sum + bow
-    else:
-        pass
-    
-print('paper:', paper_needed)
-print('ribbon:', ribbon_needed)
+    for line in lines:
+        measurements = line.split('x')
+        if len(measurements) == 3:
+            l, w, h = map(int, measurements)
+            surface_area = 2*(l*w) + 2*(w*h) + 2*(h*l)
+            extra = min((l*w), (w*h), (h*l))
+            paper_needed += surface_area + extra
+
+            # ribbon, sum of 2 smallest measurements and add l*w*h 
+            smallest_sum = (2*l) + (2*w) + (2*h) - (max(l, w, h) * 2) 
+            bow = (l*w*h)
+            ribbon_needed += smallest_sum + bow
+        else:
+            pass
+        
+    print('paper:', paper_needed)
+    print('ribbon:', ribbon_needed)
+
+part1_and2()
